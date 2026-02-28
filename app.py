751a828c1afc67fbe7deb54d936c8b7a793a3e48
Expedito de Paulo <142537791+9313TaylDe@users.noreply.github.com>
2026-02-28 11:02:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,11 @@ def Login():
     
     for user in adm_login + users_login:
         if user["email"] == email and user["senha"] == senha:
-            return jsonify({"success":True, "nome":user["nome"],"email":user["email"]})
+            return jsonify({
+    "success": True,
+    "nome": nome,
+    "email": email
+})
     return jsonify({"success":False, "message":"E-mail ou senha inválidos"})
         
 @app.route("/new", methods=["POST"])
@@ -37,7 +41,11 @@ def New():
         
     }
     users_login.append(nova_conta)
-    return jsonify({"success":True, "nome":nome["nome"],"email":email["email"]})
+    return jsonify({
+    "success": True,
+    "nome": nome,
+    "email": email
+})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
