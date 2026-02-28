ce089bd0d24573efc1e52268003862db4f16c004
Expedito de Paulo <142537791+9313TaylDe@users.noreply.github.com>
2026-02-28 11:02:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -47,5 +47,7 @@ def New():
     "email": email
 })
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
