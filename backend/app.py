d58c9677e043cf7e8be67c230455d9ec323df91d
Expedito de Paulo <142537791+9313TaylDe@users.noreply.github.com>
2026-02-28 11:03:26 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import cors from "cors";
-app.use(cors({
-  origin: "https://meu-portifolio-six-iota.vercel.app",
-  credentials: true,
-}));
+import os
 
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, resources={
+    r"/*": {
+        "origins": "https://meu-portifolio-six-iota.vercel.app"
+    }
+})
 
 adm_login = [
     {"email": "expeditotaylor@gmailcom", 
@@ -15,10 +16,7 @@ adm_login = [
      "nome":"expedito"}
 ]
 
-users_login = [
-  
-]
-
+users_login = []
 
 @app.route("/login", methods=["POST"])
 def Login():
@@ -28,22 +26,38 @@ def Login():
     
     for user in adm_login + users_login:
         if user["email"] == email and user["senha"] == senha:
-            return jsonify({"success":True, "nome":user["nome"],"email":user["email"]})
-    return jsonify({"success":False, "message":"E-mail ou senha inválidos"})
-        
+            return jsonify({
+                "success": True,
+                "nome": user["nome"],
+                "email": user["email"]
+            })
+            
+    return jsonify({
+        "success": False,
+        "message": "E-mail ou senha inválidos"
+    })
+
 @app.route("/new", methods=["POST"])
 def New():
     dados = request.json
     email = dados.get("email")
     senha = dados.get("senha")
     nome = dados.get("nome")
+
     nova_conta = {
-        "email": email,"senha" : senha, "nome": nome
-        
+        "email": email,
+        "senha": senha,
+        "nome": nome
     }
+
     users_login.append(nova_conta)
-    return jsonify({"success":True, "nome":nome["nome"],"email":email["email"]})
+
+    return jsonify({
+        "success": True,
+        "nome": nome,
+        "email": email
+    })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
